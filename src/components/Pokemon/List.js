532925380c9beb62f27ea8d56fc89bb75b38a89a
Zoumbar <esteban.ouvre@devinci.fr>
2019-11-03 22:30:42 +0100

    PokeApi / Axios

--- src/components/Pokemon/List.js
+++ src/components/Pokemon/List.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react'
 import Cell from './Cell'
+import axions from 'axios'
 
 export default class List extends Component {
+    state= {
+        url:'https://pokeapi.co/api/v2/pokemon',
+        pokemon: null
+    }
+ 
+    async componentDidMount() {
+        const res = axios.get(this.state.url);
+        this.setState({pokemon: res.data['results']});
+    }
+
     render() {
         return (
-            <div>
+            <div className="row">
                 <Cell/>
                 <Cell/>
                 <Cell/>
